38ef57e53935394fd332a8e215c9b6038f9181fc
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2023-11-16 12:19:36 +0600

    update and delete movie api created

--- server/Controller/MovieController.js
+++ server/Controller/MovieController.js
@@ -21,7 +21,7 @@ export const createAMovieController = async (req, res) => {
         });
         if (existMovie) {
             res.status(409).json({
-                message: "Movie already available in the system"
+                message: "This movie title already available in the system"
             })
         } else {
             const movie = await prisma.movie.create({
@@ -105,3 +105,67 @@ export const getAMovieController = async (req, res) => {
         })
     }
 };
+
+// Update a movie --> Controller function to update a movie
+export const updateAMovieController = async (req, res) => {
+    try {
+        const movie_id = req.params.id;
+        const {user_id, title, released_year, likes, dislikes} = req.body;
+        const movie = await prisma.movie.findFirst({ where: { id: Number(movie_id) }});
+        if (!movie) {
+            return res.status(400).json({
+                message: "No movie available by this id"
+            })
+        }
+        const updatedMovie = await prisma.movie.update({ 
+            where: { 
+                id: Number(movie_id)
+            },
+            data: {
+                user_id,
+                title,
+                released_year,
+                likes,
+                dislikes
+            }
+        })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                updatedMovie
+            }
+        })       
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'failed',
+            message: "Error while upating movie info"
+        })       
+    }
+};
+
+// Delete a movie --> Controller function to delete a movie
+export const deleteAMovieController = async (req, res) => {
+    try {
+        const movie_id = req.params.id;
+        const movie = await prisma.movie.findFirst({ where: { id: Number(movie_id) }});
+        if (!movie) {
+            return res.status(400).json({
+                message: "No movie found by this id"
+            })
+        }
+        await prisma.movie.delete({ where: { id: Number(movie_id) }});
+        res.status(200).json({
+            status: 'success',
+            message: "Successfully deleted"
+        })       
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: "failed",
+            message: "Error while deleting the movie"
+        })        
+    }
+}
--- server/routes/MovieRoutes.js
+++ server/routes/MovieRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { createAMovieController, getAMovieController, getAllMoviesController } from '../Controller/MovieController.js';
+import { 
+    createAMovieController, 
+    deleteAMovieController, 
+    getAMovieController, 
+    getAllMoviesController, 
+    updateAMovieController 
+} from '../Controller/MovieController.js';
 
 const router = express.Router();
 
 router.post('/create', createAMovieController);
 router.get('/fetch/movies', getAllMoviesController);
 router.get('/fetch/:id', getAMovieController);
+router.patch('/modify/:id', updateAMovieController);
+router.delete('/remove/:id', deleteAMovieController);
 
 export default router;
